3d866b5f34138fc6f76c36aa1b8928f3f6bc76d9
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-03-25 23:31:29 +0300

    Add project files.

---
+++ BatuMvc/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BatuMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendData(Data data)
+        {
+
+            return View();
+        }
+    }
+
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
