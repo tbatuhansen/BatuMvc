fa4dffb8aec1ea28e5b1e5cd6a0b5bb3344d2f51
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-08-24 16:16:40 +0300

    Edit HomeController

--- BatuMvc/Controllers/HomeController.cs
+++ BatuMvc/Controllers/HomeController.cs
@@ -15,21 +15,16 @@ namespace BatuMvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         [HttpPost]
         public ActionResult SendData(Data data)
         {
-
             return View();
         }
     }
